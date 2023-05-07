9d89c514d9b4bbfd9b065e92ecb03e8f27f93c96
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2023-05-07 14:23:33 -0300

    Added http methods to UserResource and added an exception handler

--- src/main/java/com/webservices/warehouse/services/UserService.java
+++ src/main/java/com/webservices/warehouse/services/UserService.java
@@ -2,11 +2,18 @@ package com.webservices.warehouse.services;
 
 import com.webservices.warehouse.entities.User;
 import com.webservices.warehouse.repositories.UserRepository;
+import com.webservices.warehouse.services.exceptions.DatabaseException;
+import com.webservices.warehouse.services.exceptions.ResourceNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -20,6 +27,37 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setPhone(obj.getPhone());
+        entity.setEmail(obj.getEmail());
     }
 }

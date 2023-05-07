642773118cde96497a79f3724ae0154367dcb811
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2023-05-07 10:30:08 -0300

    Added entity Payment, association with entity Product and db seeding

--- src/main/java/com/webservices/warehouse/configs/TestConfig.java
+++ src/main/java/com/webservices/warehouse/configs/TestConfig.java
@@ -3,6 +3,7 @@ package com.webservices.warehouse.configs;
 import com.webservices.warehouse.entities.Category;
 import com.webservices.warehouse.entities.Order;
 import com.webservices.warehouse.entities.OrderItem;
+import com.webservices.warehouse.entities.Payment;
 import com.webservices.warehouse.entities.User;
 import com.webservices.warehouse.entities.Product;
 import com.webservices.warehouse.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
